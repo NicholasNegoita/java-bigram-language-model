8373eea40b3117dfecc70ff6d46b92a4c4acc6bc
NicholasNegoita <nicholas.negoita@gmail.com>
2026-05-05 18:57:53 -0400

    Fix formatting issues and clean up imports in Bigram.java

--- Bigram.java
+++ Bigram.java
@@ -1,10 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Scanner;
+import java.util.Map;
 
 public class Bigram {
 	
